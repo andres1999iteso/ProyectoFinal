a48f9a33950d45b75800a9b7477033ce868fc7a8
Andrés Ramírez Villanueva <if726061@iteso.mx>
2023-05-08 20:46:21 -0600

    Separamos las funiones del archivo de data

--- data.py
+++ data.py
@@ -1,37 +1,8 @@
-import pytz
-from datetime import datetime 
 import pandas as pd
-import MetaTrader5 as mt5
+import functions as fn
 
 indicador = pd.read_csv('./files/Gross Domestic Product Annualized.csv')
 
-def descarga_data(tickers):
+USDMXN = fn.descarga_data(["USDMXN"])
 
-    mt5.initialize()
-    timezone = pytz.timezone("Etc/UTC")
-    utc_from = datetime(2022,6,1,tzinfo=timezone)
-    final = {}
-    
-    for i in tickers:
-        
-        data=(mt5.copy_rates_from(i,mt5.TIMEFRAME_M15,utc_from,85000))
-        
-        data=pd.DataFrame(data)
-        
-        data['time'] = pd.to_datetime(data['time'], unit='s')
-        
-        data = data[(data['time'] >= '2020-01-01') &  (data['time'] <='2022-02-01')]
-        
-        final[i]=data
-        
-        final[i].to_csv('files/'+i+'.csv')
-        
-    mt5.shutdown()
-    
-    return final
-
-
-g=descarga_data(["USDMXN"])
-
-
-f=descarga_data(["EURUSD"])
+EURUSD = fn. descarga_data(["EURUSD"]) 
--- functions.py
+++ functions.py
@@ -1,10 +1,27 @@
+import pytz
+from datetime import datetime 
+import pandas as pd
+import MetaTrader5 as mt5
 
-"""
-# -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
-# -- script: functions.py : python script with general functions                                         -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
-# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
-# -- --------------------------------------------------------------------------------------------------- -- #
-"""
+def descarga_data(tickers):
+
+    mt5.initialize()
+    timezone = pytz.timezone("Etc/UTC")
+    utc_from = datetime(2022,6,1,tzinfo=timezone)
+    final = {}
+    
+    for i in tickers:
+        
+        data=(mt5.copy_rates_from(i,mt5.TIMEFRAME_M15,utc_from,85000))
+        
+        data=pd.DataFrame(data)
+        
+        data['time'] = pd.to_datetime(data['time'], unit='s')
+        
+        data = data[(data['time'] >= '2020-01-01') &  (data['time'] <='2022-02-01')]
+        
+        final[i]=data        
+       
+    mt5.shutdown()
+    
+    return final
