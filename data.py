963a7b4915e00193d02a7e8658d65035814dbf28
Andrés Ramírez Villanueva <if726061@iteso.mx>
2023-05-08 21:07:13 -0600

    Separamos periodos de train y test

--- data.py
+++ data.py
@@ -3,6 +3,14 @@ import functions as fn
 
 indicador = pd.read_csv('./files/Gross Domestic Product Annualized.csv')
 
-USDMXN = fn.descarga_data(["USDMXN"])
+USDMXN = fn.descarga_data(["USDMXN"])['USDMXN']
 
-EURUSD = fn. descarga_data(["EURUSD"]) 
+USDMXN_train = USDMXN[(USDMXN['time'] >= '2020-01-01') & (USDMXN['time'] <='2021-01-01')]
+
+USDMXN_test = USDMXN[(USDMXN['time'] >= '2021-02-01') & (USDMXN['time'] <='2022-02-01')]
+
+EURUSD = fn. descarga_data(["EURUSD"])["EURUSD"]
+
+EURUSD_train = EURUSD[(EURUSD['time'] >= '2020-01-01') & (EURUSD['time'] <='2021-01-01')]
+
+EURUSD_test = EURUSD[(EURUSD['time'] >= '2021-02-01') & (EURUSD['time'] <='2022-02-01')] 
