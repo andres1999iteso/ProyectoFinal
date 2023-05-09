05ae5bf07be60c5b26beede6bc7070637fd74765
Andrés Ramírez Villanueva <if726061@iteso.mx>
2023-05-08 20:42:20 -0600

    modificacion a funcion de datos, tenia errores

--- data.py
+++ data.py
@@ -1,45 +1,37 @@
-
-"""
-# -- --------------------------------------------------------------------------------------------------- -- #
-# -- project: A SHORT DESCRIPTION OF THE PROJECT                                                         -- #
-# -- script: data.py : python script for data collection                                                 -- #
-# -- author: YOUR GITHUB USER NAME                                                                       -- #
-# -- license: THE LICENSE TYPE AS STATED IN THE REPOSITORY                                               -- #
-# -- repository: YOUR REPOSITORY URL                                                                     -- #
-# -- --------------------------------------------------------------------------------------------------- -- #
-"""
-
-dict_test = {'key_a': 'a', 'key_b': 'b'}
-
-import datetime
 import pytz
-from datetime import datime 
+from datetime import datetime 
 import pandas as pd
 import MetaTrader5 as mt5
 
-indice = pd.read_csv(r'/Users/gabrielaortiz/Documents/ITESO/Micro y sistemas de traiding/ProyectoFinal/files/Gross Domestic Product Annualized.csv')
+indicador = pd.read_csv('./files/Gross Domestic Product Annualized.csv')
 
-def f_import_mt5(list_tickers):
+def descarga_data(tickers):
+
     mt5.initialize()
     timezone = pytz.timezone("Etc/UTC")
-    utc_from = datetime(2020,1,1,tzinfo=timezone)
-    utc_to = datetime(2022,2,1,tzinfo=timezone)
-    mt5_rates = {}
-    for i in list_tickers:
-        rates=(mt5.copy_rates_from(i,mt5.TIMEFRAME_M15,utc_from,85000,utc_to))
-        rates=pd.DataFrame(rates)
-        rates['time'] = pd.to_datetime(rates['time'], unit='s')
-        #rates.to_csv('')
-        mt5_rates[i]=rates
-        mt5_rates[i].to_csv('files/'+i+'.csv')
+    utc_from = datetime(2022,6,1,tzinfo=timezone)
+    final = {}
+    
+    for i in tickers:
+        
+        data=(mt5.copy_rates_from(i,mt5.TIMEFRAME_M15,utc_from,85000))
+        
+        data=pd.DataFrame(data)
+        
+        data['time'] = pd.to_datetime(data['time'], unit='s')
+        
+        data = data[(data['time'] >= '2020-01-01') &  (data['time'] <='2022-02-01')]
+        
+        final[i]=data
+        
+        final[i].to_csv('files/'+i+'.csv')
+        
     mt5.shutdown()
-    return mt5_rates
+    
+    return final
 
-list_tickers = ['USDMXN','USDEUR']
-historico = f_import_mt5(list_tickers)
-usdmxn_prices = historical_data['USDMXN']
-usdeur_prices = historical_data['USDEUR']
 
-print(indice.head())
-print(usdmxn_prices.head())
-print(usdeur_prices.head())
+g=descarga_data(["USDMXN"])
+
+
+f=descarga_data(["EURUSD"])
